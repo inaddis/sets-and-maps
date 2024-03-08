3a1076a36443f822ea149ad981d464d7b080ed3b
inaddis <145146147+inaddis@users.noreply.github.com>
2024-03-07 21:34:49 -0700

    Create cow.hpp

---
+++ cow.hpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <set> 
+#include <string>
+
+
+class Cow {
+  private: 
+      std::string name;
+
+  public: 
+      Cow(const std::string& name) : name(name) {}
+
+      
+};
