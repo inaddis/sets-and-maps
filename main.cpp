81b9dc7d0d20077180cd9d98d6ab95b2d71acc3c
inaddis <145146147+inaddis@users.noreply.github.com>
2024-03-07 21:56:23 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
-#include "cow.hpp"
+#include "Cow.hpp"
+#include "Ship.hpp"
+#include <vector>
 
 int main() {
-  
+  std::vector<Ship*> ships;
+
+  while (true) {
+    std::cout << "Menu: " << std::endl;
+    std::cout << "1. Create a ship\n2. Abduct a cow\n3. Self-destruct (quit program)\nEnter Choice: ";
+    int choice;
+    std::cin >> choice;
 }
