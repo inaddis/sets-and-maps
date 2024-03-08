42d19c9a9b017150cba183675847941b0c23694d
inaddis <145146147+inaddis@users.noreply.github.com>
2024-03-07 21:48:11 -0700

    Update Cow.hpp

--- Cow.hpp
+++ Cow.hpp
@@ -9,6 +9,10 @@ class Cow {
   public:   
     Cow(const std::string& name) : name(name) {}
 
+    ~Cow() {
+      std::cout << "Cow named " << name << "has been destroyed" << std::endl;
+    }
+
     friend std::ostream& operator<<(std::ostream& os, const Cow& cow) {
       os << "Cow Name: " << cow.name;
       return os;
